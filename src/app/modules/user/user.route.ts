6dd5fda49c555031e35e27427632f64059f931a5
sajjadhossain <sajjadhossain0756@gmail.com>
2025-08-05 15:52:48 +0600

    getAllUser with Role validation wiit checkAuth middleware

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import { UserControllers } from "./user.controller";
 import { validationRequest } from "../../middlewares/validationRequest";
 import { createUserZodSchema } from "./user.validation";
+import { checkAuth } from "../../middlewares/checkAuth";
+import { Role } from "./user.interface";
 
 
 const router = Router();
@@ -9,7 +11,7 @@ const router = Router();
 router.post("/register",
     validationRequest(createUserZodSchema),
     UserControllers.createUser);
-router.get("/all-users", UserControllers.getAllUsers);    
+router.get("/all-users", checkAuth(Role.ADMIN), UserControllers.getAllUsers);
 
 
 export const userRoutes = router;
